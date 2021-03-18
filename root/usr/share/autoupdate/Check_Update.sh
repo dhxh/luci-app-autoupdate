405f395894bf6953095c3b5c8f9451b8c3484548
dhxh <846703029@qq.com>
2021-03-18 23:32:20 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -7,14 +7,15 @@ if [ ! -f /bin/AutoUpdate.sh ];then
 	echo "未检测到 /bin/AutoUpdate.sh" > /tmp/cloud_version
 	exit
 fi
-CURRENT_DEVICE="$(awk 'NR==3' /etc/openwrt_info)"
+Firmware_Type="$(awk 'NR==4' /etc/openwrt_info)"
+Firmware_COMP1="$(awk 'NR==5' /etc/openwrt_info)"
+Firmware_COMP2="$(awk 'NR==6' /etc/openwrt_info)"
 [[ -z "${CURRENT_DEVICE}" ]] && CURRENT_DEVICE="$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')"
 Github="$(awk 'NR==2' /etc/openwrt_info)"
 [[ -z "${Github}" ]] && exit
 Author="${Github##*com/}"
 Github_Tags="https://api.github.com/repos/${Author}/releases/latest"
 wget -q ${Github_Tags} -O - > /tmp/Github_Tags
-Firmware_Type="$(awk 'NR==4' /etc/openwrt_info)"
 case ${CURRENT_DEVICE} in
 x86_64)
 	if [ -d /sys/firmware/efi ];then
@@ -30,16 +31,28 @@ x86_64)
 	BOOT_Type=""
 ;;
 esac
-Cloud_Version="$(cat /tmp/Github_Tags | egrep -o "AutoBuild-${CURRENT_DEVICE}-R[0-9]+.[0-9]+.[0-9]+.[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o 'R[0-9]+.[0-9]+.[0-9]+.[0-9]+')"
-CURRENT_Version="$(awk 'NR==1' /etc/openwrt_info)"
-if [[ ! -z "${Cloud_Version}" ]];then
-	if [[ "${CURRENT_Version}" == "${Cloud_Version}" ]];then
-		Checked_Type="已是最新"
-	else
-		Checked_Type="可更新"
+Cloud_Ver="$(cat /tmp/Github_Tags | egrep -o "${Firmware_COMP1}-${Firmware_COMP2}-${CURRENT_DEVICE}-[0-9]+.*?[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-zA-Z0-9_-]+.*?[0-9]+')"
+echo "cloud: ${Cloud_Ver}"
+upda_day="${Cloud_Ver##*${CURRENT_DEVICE}-}"
+upda_day1="${upda_day%-*}"
+upda_day2="${upda_day#*-}"
+GET_Ver="${upda_day1}${upda_day2}"
+GET_Version="${upda_day1}-${upda_day2}"
+CURRENT_day1="$(awk 'NR==7' /etc/openwrt_info)"
+CURRENT_day2="$(awk 'NR==8' /etc/openwrt_info)"
+CURRENT_Version="${CURRENT_day1}${CURRENT_day2}"
+if [[ ! -z "${Cloud_Ver}" ]];then
+    if [[ "${CURRENT_Version}" -gt "${GET_Ver}" ]];then
+        Checked_Type="当前版本大于Github版本"
+		echo "${GET_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version
+    elif [[ "${CURRENT_Version}" -eq "${GET_Ver}" ]];then
+        Checked_Type="已是最新"
+		echo "${GET_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version
+	elif [[ "${CURRENT_Version}" -lt "${GET_Ver}" ]];then
+	    Checked_Type="可更新"
+		echo "${GET_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version
 	fi
-	echo "${Cloud_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version
 else
-	echo "未知" > /tmp/cloud_version
+    echo "未知" > /tmp/cloud_version
 fi
 exit
